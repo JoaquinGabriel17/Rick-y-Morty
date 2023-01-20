8c02cb83ef9141b4727255ddae58437588f72e89
JoaquinGabriel17 <joaquingabriel3@hotmail.com>
2023-01-20 19:33:19 -0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Cards from './componentes/cards/cards.jsx';
+import { useState } from 'react';
+import Nav from './componentes/Nav/Nav.jsx';
+import { Routes,Route } from 'react-router-dom';
+import About from './componentes/About/About';
+import Detail from './componentes/Detail/Detail';
+import Form from './componentes/Form/Form';
+import Favorites from './componentes/favorites/Favorites';
 
 function App() {
+
+  const [characters, setCharacters] = useState([])
+
+  const onSearch = (character) =>{
+    fetch(`https://rickandmortyapi.com/api/character/${character}`)
+      .then((response) => response.json())
+      .then((data) => {
+         if (data.name) {
+            setCharacters((oldChars) => [...oldChars, data]);
+         } else {
+            window.alert('No hay personajes con ese ID');
+         }
+      });
+  }
+
+  const onClose = (id) => {
+    setCharacters(
+      characters.filter(character => character.id !== id)
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Nav onSearch={onSearch} ></Nav>
+    
+
+        <Routes>
+          <Route path='/favorites' element={<Favorites/>} />
+          <Route path="/" element={<Form/>} />
+          <Route path='/home' element={<Cards onClose={onClose} characters={characters} />} /> 
+          <Route path='/about' element={<About/>} />
+          <Route path='/detail/:detailID' element={<Detail/>} />
+      </Routes>
+      
+      
+      
+
     </div>
   );
 }
