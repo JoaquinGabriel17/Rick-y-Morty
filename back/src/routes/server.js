e4373ecd2c9fe6d1f7486df90e73265df036df6c
JoaquinGabriel17 <joaquingabriel3@hotmail.com>
2023-03-15 11:39:51 -0300

    dev

--- back/src/routes/server.js
+++ back/src/routes/server.js
@@ -1,22 +1,3 @@
-// const http = require('http');
-// const getCharById = require('../controllers/getCharById');
-// const getCharDetail = require('../controllers/getCharDetail');
-
-// http.createServer((req, res) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-
-//     let id = req.url.split('/').at(-1);
-
-//     if(req.url.includes('onsearch')){
-//         getCharById(res, id)
-//     }
-
-//     if(req.url.includes('detail')){
-//         getCharDetail(res, id)
-//     }
-
-// }).listen(3001, 'localhost');
-
 
 const express = require('express')
 const app = express();
--- src/index.js
+++ src/index.js
@@ -6,14 +6,15 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import  store  from './redux/store';
 import { Provider } from 'react-redux';
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://localhost:3001/rickandmorty'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
-      
-      
     </BrowserRouter>
   </Provider>
 );
